ae63db21fa31c5b940698ab6abe93cb71f078181
Kammeron Nhieu <kammeronnhieu@gmail.com>
2018-09-18 15:57:39 -0700

    api calling

--- myTravel/MapVC.swift
+++ myTravel/MapVC.swift
@@ -14,24 +14,34 @@ class MapVC: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    let key = "AIzaSyBwiQRbzK3aLVLy34fjHKoEaJxkUhpdvv8"
+    var thisLatitude = "37.375489318087"
+    var thisLongitude = "-121.910261964237"
+    var thisRadius = "5000"
     let locationManager = CLLocationManager()
     var myLocation: CLLocationCoordinate2D?
+    var thisType = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
+        print("ViewDidLoad: \(thisLatitude)")
+        print("ViewDidLoad: \(thisLongitude)")
+        getPlaces()
+        print("after lat: \(thisLatitude)")
+        print("after long: \(thisLatitude)")
         // Do any additional setup after loading the view.
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0]
         myLocation = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        thisLatitude = String(location.coordinate.latitude)
+        thisLongitude = String(location.coordinate.longitude)
         
-        print(myLocation)
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
@@ -44,14 +54,28 @@ class MapVC: UIViewController, CLLocationManagerDelegate {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // get api functions
+    
+    func getPlaces() {
+        print(thisLatitude)
+        print(thisLongitude)
+        let url = URL(string: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(thisLatitude),\(thisLongitude)&radius=\(thisRadius)&types=point_of_interest&key=\(key)")
+        print(url)
+        let session = URLSession.shared
+        let task = session.dataTask(with: url!, completionHandler: {
+            data, response, error in
+            do {
+                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                    print(jsonResult)
+                }
+            } catch {
+                print(error)
+            }
+        })
+        // execute the task and then wait for the response
+        // to run the completion handler. This is async!
+        task.resume()
     }
-    */
 
 }
