f140d32ebab1205d95a79cd7bf0c29b89844e67b
Tenju Paul <tenju@tenjus-macbook-air.local>
2018-09-18 14:42:53 -0700

    MyTravel VC

--- myTravel/MyTravelViewController.swift
+++ myTravel/MyTravelViewController.swift
@@ -14,7 +14,7 @@ class MyTravelViewController: UITableViewController {
     @IBOutlet weak var destinationTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("MyTravelVC did load")
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,15 +24,7 @@ class MyTravelViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+  
 
 
 }
