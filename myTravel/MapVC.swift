e6312ebfda066442561300a6cffb196ea42ddc17
Kammeron Nhieu <kammeronnhieu@gmail.com>
2018-09-18 18:10:53 -0700

    Added global changes

--- myTravel/MapVC.swift
+++ myTravel/MapVC.swift
@@ -16,11 +16,9 @@ class MapVC: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var tableView: UITableView!
     
     let key = "AIzaSyBwiQRbzK3aLVLy34fjHKoEaJxkUhpdvv8"
-    var thisLatitude = "37.375489318087"
-    var thisLongitude = "-121.910261964237"
+    var thisLatitude = ""
+    var thisLongitude = ""
     var thisRadius = "5000"
-    let locationManager = CLLocationManager()
-    var myLocation: CLLocationCoordinate2D?
     var thisType = ""
     var locResults: [NSDictionary] = [NSDictionary]()
     
@@ -30,30 +28,11 @@ class MapVC: UIViewController, CLLocationManagerDelegate {
         tableView.dataSource = self
         tableView.rowHeight = 100
         
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.startUpdatingLocation()
-        print("ViewDidLoad: \(thisLatitude)")
-        print("ViewDidLoad: \(thisLongitude)")
+        thisLatitude = Global.shared.latitude
+        thisLongitude = Global.shared.longitude
         getPlaces()
-        print("after lat: \(thisLatitude)")
-        print("after long: \(thisLatitude)")
         // Do any additional setup after loading the view.
     }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations[0]
-        myLocation = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
-        thisLatitude = String(location.coordinate.latitude)
-        thisLongitude = String(location.coordinate.longitude)
-        
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print(error)
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
